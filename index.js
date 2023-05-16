e3c9ed9becbff9c355f32b6b06c9fb9da65c60c8
tuhin_4ever <tuhin4ever19@gmail.com>
2023-05-16 23:12:26 +0600

    await client.connect() comment for work perfectly

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const verifyJWT = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
